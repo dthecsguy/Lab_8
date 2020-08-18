1a2e8ab619d3f3e1241723c876a0c1fb9a0cd0b0
dthecsguy <50031416+dthecsguy@users.noreply.github.com>
2020-08-17 19:53:34 -0700

    Update basicPWM.h

--- header/basicPWM.h
+++ header/basicPWM.h
@@ -4,15 +4,10 @@
 #define A1 (A & 0x02)
 #define A0 (A & 0x01)
 
-#define C (0x04)
-#define D (0x02)
-#define E (0x01)
-
 #define C_FREQ 261.63
 #define D_FREQ 293.66
 #define E_FREQ 329.63
 
-#define INPUTS (0x00 | A2 | A1 | A0)
 #define HLD_T 4 //how long to hold button
 
 enum States {WAIT, C_PRESSED, D_PRESSED, E_PRESSED, C_HELD, D_HELD, E_HELD} state;
@@ -21,56 +16,57 @@ static unsigned char cnt = 0;
 void tick(){
    switch(state){  //Transitions
 		case WAIT:
-			if (INPUTS == C){
+			if (A2){
 				state = C_PRESSED;
 			}
-			else if (INPUTS == D){
+			else if (A1){
 				state = D_PRESSED;
 			}
-			else if (INPUTS == E){
+			else if (A0){
 				state = E_PRESSED;
 			}
 			else {
 				state = WAIT;
 			}
 			break;
+		   
 		case C_PRESSED:
 			if (cnt < HLD_T){
-				state = (INPUTS == C) ? C_PRESSED : WAIT;
+				state = (A2) ? C_PRESSED : WAIT;
 			}
 			else {
-				state = (INPUTS == C) ? C_HELD : WAIT;
+				state = (A2) ? C_HELD : WAIT;
 			}
 			break;
 		
 		case D_PRESSED:
 			if (cnt < HLD_T){
-				state = (INPUTS == D) ? D_PRESSED : WAIT;
+				state = (A1) ? D_PRESSED : WAIT;
 			}
 			else {
-				state = (INPUTS == D) ? D_HELD : WAIT;
+				state = (A1) ? D_HELD : WAIT;
 			}
 			break;
 		
 		case E_PRESSED:
 			if (cnt < HLD_T){
-				state = (INPUTS == E) ? E_PRESSED : WAIT;
+				state = (A0) ? E_PRESSED : WAIT;
 			}
 			else {
-				state = (INPUTS == E) ? E_HELD : WAIT;
+				state = (A0) ? E_HELD : WAIT;
 			}
 			break;
 		
 		case C_HELD:
-			state = (INPUTS == C) ? C_HELD : WAIT; 
+			state = (A2) ? C_HELD : WAIT; 
 			break;
 		
 		case D_HELD:
-			state = (INPUTS == D) ? D_HELD : WAIT;
+			state = (A1) ? D_HELD : WAIT;
 			break;
 		
 		case E_HELD:
-			state = (INPUTS == E) ? E_HELD : WAIT;
+			state = (A0) ? E_HELD : WAIT;
 			break;
 		
    }
