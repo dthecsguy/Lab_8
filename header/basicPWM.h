9c5fbd7e9f2c1d2c454e59110d9f98ba418df815
Demarcus Sales <dthecsguy@gmail.com>
2020-08-17 18:01:53 -0700

    lets test

--- header/basicPWM.h
+++ header/basicPWM.h
@@ -1,11 +1,102 @@
-#define INPUTS (~PINA & 0x07)
 #define C (~PINA & 0x04)
 #define D (~PINA & 0x02)
 #define E (~PINA & 0x01)
+
+#define C_FREQ 261.63
+#define D_FREQ 293.66
+#define E_FREQ 329.63
+
+#define INPUTS (C | D | E)
+#define HLD_T 4 //how long to hold button
 
 enum States {WAIT, C_PRESSED, D_PRESSED E_PRESSED, C_HELD, D_HELD, E_HELD} state;
 
 void tick(){
    switch(state){  //Transitions
+		case WAIT:
+			if (INPUTS == C){
+				state = C_PRESSED;
+			}
+			else if (INPUTS == D){
+				state = D_PRESSED;
+			}
+			else if (INPUTS == E){
+				state = E_PRESSED;
+			}
+			else {
+				state = WAIT;
+			}
+			break;
+		case C_PRESSED:
+			if (ticks < HLD_T){
+				state = (INPUTS == C) ? C_PRESSED : WAIT;
+			}
+			else {
+				state = (INPUTS == C) ? C_HELD : WAIT;
+			}
+			break;
+		
+		case D_PRESSED:
+			if (ticks < HLD_T){
+				state = (INPUTS == D) ? D_PRESSED : WAIT;
+			}
+			else {
+				state = (INPUTS == D) ? D_HELD : WAIT;
+			}
+			break;
+		
+		case E_PRESSED:
+			if (ticks < HLD_T){
+				state = (INPUTS == E) ? E_PRESSED : WAIT;
+			}
+			else {
+				state = (INPUTS == E) ? E_HELD : WAIT;
+			}
+			break;
+		
+		case C_HELD:
+			state = (INPUTS == C) ? C_HELD : WAIT; 
+			break;
+		
+		case D_HELD:
+			state = (INPUTS == D) ? D_HELD : WAIT;
+			break;
+		
+		case E_HELD:
+			state = (INPUTS == E) ? E_HELD : WAIT;
+			break;
+		
+   }
+   switch(state){  //Actions
+		case WAIT:
+			tick = 0;
+			break;
+			
+		case C_PRESSED:
+			tick++;
+			break;
+		
+		case D_PRESSED:
+			tick++;
+			break;
+		
+		case E_PRESSED:
+			tick++;
+			break;
+		
+		case C_HELD:
+			tick = 0;
+			set_PWM(C_FREQ);
+			break;
+		
+		case D_HELD:
+			tick = 0;
+			set_PWM(D_FREQ);
+			break;
+		
+		case E_HELD:
+			tick = 0;
+			set_PWM(E_FREQ);
+			break;
    }
 }
--- source/main.c
+++ source/main.c
@@ -8,18 +8,68 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include "timer.h"
+#include "basicPWM.h"
+
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
+volatile unsigned char TimerFlag = 0;
+static unsigned char tick = 0;
+void TimerISR() { TimerFlag = 1; }
+
+void set_PWM(double frequency) {
+	static double current_frequency;
+	
+	if (frequency != current_frequency) {
+		if (!frequency) { TCCR3B &= 0x08; }
+		else { TCCR3B |= 0x03; }
+		
+		if (frequency < 0.954) { OCR3A = 0xFFFF; }
+		
+		else if (frequency > 31250) { OCR3A = 0x0000; }
+		
+		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
+		
+		TCNT3 = 0;
+		current_frequency = frequency;
+	}
+}
+
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+	
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	
+	set_PWM(0);
+}
+
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
+
 
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00;    PORTA = 0xFF;
     DDRB = 0xFF;    PORTB = 0x00;
+	
+	TimerSet(50);
+	TimerOn();
+	
+	PWM_on();
     
     /* Insert your solution below */
     while (1) {
 
+		tick();
+		while(!TimerFlag){}
+		TimerFlag = 0;
+		
     }
+	
+	PWM_off();
     return 1;
 }
